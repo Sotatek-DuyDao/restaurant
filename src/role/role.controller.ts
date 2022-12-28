b353791e11f9fbf787cac3e3099a042e0150d36d
Sotatek-DuyDao <duy.dao@sotatek.com>
2022-12-28 18:50:59 +0700

    Init

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -3,6 +3,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 class RoleController {
   async createRole(req: FastifyRequest, res: FastifyReply) {
     req.log.info(req.body);
+
     return req.body;
   }
 }
